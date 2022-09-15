f528ce7ef95ac15b351ad0ae4852258581a61949
kaskjfd <105328163+kaskjfd@users.noreply.github.com>
2022-09-15 16:25:04 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  count = 0;
+  toggle = 1;
+  delay(1000); // wait for 1000 milliseconds
+}
+
+void loop() {
+  while(1) {
+    if (count == 12) {
+      break;
+    }
+    toggle = toggle_state(toggle); //toggle LED value.
+    count += 1;
+    Serial.println(count);
+    digitalWrite(PIN_LED, toggle); // update LED status.
+    delay(100); // wait for 1000 milliseconds
+    } // infinite loop
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
